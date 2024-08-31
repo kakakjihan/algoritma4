6a79b54c7d734090667771f1b3d9b11057fef441
kakakjihan <jihansahfitri6@gmail.com>
2024-08-31 12:25:58 +0700

    pernyataan foreach

---
+++ 09 pernyataan foreach.php
@@ -0,0 +1,6 @@
+<?php
+$makanan = array('nasi goreng', 'ayam bakar', 'mie aceh','ayam penyet');
+foreach ($makanan as $w) {
+  echo "makanan: $w<hr>";
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
