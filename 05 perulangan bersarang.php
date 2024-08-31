42b931e0da98200a15caaf958426fb37bb829925
kakakjihan <jihansahfitri6@gmail.com>
2024-08-31 11:53:37 +0700

    perulangan bersarang

---
+++ 05 perulangan bersarang.php
@@ -0,0 +1,9 @@
+<?php
+for($i = 0; $i < 3; $i++){
+    for($j = 0; $j < 5; $j++){
+        echo "Ini perulangan ke ($i, $j)<br><hr>";
+    }
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
+?>
+
