64293bd31529636623bd50b489a5a7210b3b256a
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:26:10 +0700

    penggunaan break

---
+++ 25 Penggunaan break.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    if ($i === 7) {
+        break;
+    }
+    echo $i . " ";
+}
+// Output: 0 1 2 3 4 5 6 7
+print_r("<P><strong>by :jihan syafitri</strong>");
