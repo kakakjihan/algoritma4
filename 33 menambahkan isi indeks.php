267cf7c4130ef5e09b2e6111e499e2421b7b0e01
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:54:17 +0700

    menambahkan isi indeks

---
+++ 33 menambahkan isi indeks.php
@@ -0,0 +1,17 @@
+<?php
+// membuat array
+$user = [
+    "novla",
+    "sasa",
+    "jihansyafitri"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "jihan";
+
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+print_r("<P><strong>by :jihan syafitri</strong>");
+?>
