43f8ae2f92c806a343fd752a98d23ae747e06029
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 20:10:21 +0700

    cara penulisan foreach

---
+++ 35 Cara Penulisan Foreach Pada PHP.php
@@ -0,0 +1,6 @@
+<?php
+$bulan = array ('Januari', 'Februari', 'Maret','april');
+foreach ($bulan as $nama_bulan) {
+	echo $nama_bulan . '<br/><hr>';
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
