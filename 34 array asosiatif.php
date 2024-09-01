0e41fc6dc45d8d3565564953234a4f052c0dbf3b
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 17:00:36 +0700

    array asosiatif

---
+++ 34 array asosiatif.php
@@ -0,0 +1,13 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul dongeng" => "cinderella",
+    "penulis" => "charles perrault",
+    "view" => 128
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul dongeng"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+print_r("<P><strong>by :jihan syafitri</strong>");
