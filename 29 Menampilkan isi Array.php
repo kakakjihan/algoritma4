0088660dc57381832abbebae903b84ff2adf7076
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:38:21 +0700

    menampilkan isi array

---
+++ 29 Menampilkan isi Array.php
@@ -0,0 +1,11 @@
+<?php
+// membuat array
+$barang = ["Buku Tulis", "Penghapus", "Spidol","penggaris","stip ex"];
+
+// menampilkan isi array
+echo $barang[0]."<br><hr>";
+echo $barang[1]."<br><hr>";
+echo $barang[2]."<br><hr>";
+echo $barang[3]."<br><hr>";
+echo $barang[4]."<br><hr>";
+print_r("<P><strong>by :jihan syafitri</strong>");
