8c1be6bf42ef326e31a707e60edf044f10ad551c
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:31:33 +0700

    break pada switch

---
+++ 27 break pada switch.php
@@ -0,0 +1,18 @@
+<?php
+$buah = "apel";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "apel":
+        echo "Ini durian";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
