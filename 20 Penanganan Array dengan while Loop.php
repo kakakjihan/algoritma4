3af664c7d5bc514fe98ca9c05012e22a1f88abdb
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:07:50 +0700

    penanganan array pada while loop

---
+++ 20 Penanganan Array dengan while Loop.php
@@ -0,0 +1,10 @@
+<?php
+$food = ["fried rice", "grilled chicken", "aceh noodles"];
+$i = 0;
+
+while ($i < count($food)) {
+    echo $food[$i] . "<br><hr>";
+    $i++;
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
+?>
