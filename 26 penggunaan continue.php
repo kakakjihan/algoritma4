1a5f4793aec7cd1c3eea787b2c63ad8afe21c1cd
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:28:27 +0700

    penggunaan continue

---
+++ 26 penggunaan continue.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    if ($i % 2 === 0) {
+        continue;
+    }
+    echo $i . " ";
+}
+// Output: 1 3 5 7 9 
+print_r("<P><strong>by :jihan syafitri</strong>");
