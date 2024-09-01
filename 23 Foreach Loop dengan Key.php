af7867e7783b15854255d44d0c3640ae956d8dcd
kakakjihan <jihansahfitri6@gmail.com>
2024-09-01 16:17:28 +0700

    foreach loop dengan key

---
+++ 23 Foreach Loop dengan Key.php
@@ -0,0 +1,7 @@
+<?php
+$harga = array("apel" => 10000, "pisang" => 5000, "ceri" => 15000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah: " . $nilai . "<br><hr>";
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
