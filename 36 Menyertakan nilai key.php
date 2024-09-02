99fc6674a6ac70587ad8a4dab43ec6c81de2e9e0
kakakjihan <jihansahfitri6@gmail.com>
2024-09-02 22:42:06 +0700

    menyertakan nilai key

---
+++ 36 Menyertakan nilai key.php
@@ -0,0 +1,6 @@
+<?php
+$bulan = array ('Januari', 'Februari', 'Maret');
+foreach ($bulan as $index => $nama_bulan) {
+	echo ($index + 1) . '. ' . $nama_bulan . '<br/><hr>';
+}
+print_r("<P><strong>by :jihan syafitri</strong>");
