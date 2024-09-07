bf22e20e6030c6776558a1a972b74c219b020fa1
kakakjihan <jihansahfitri6@gmail.com>
2024-09-07 18:48:26 +0700

    membuat array

---
+++ 43 membuat array.php
@@ -0,0 +1,5 @@
+<?php
+// Menggunakan fungsi array()
+$buah = array('apel', 'jeruk', 'pisang');
+// Menggunakan tanda kurung siku []
+$warna = ['merah', 'hijau', 'biru'];
---
+++ 44 mengakses data array.php
@@ -0,0 +1,4 @@
+<?php
+echo $buah[1]; // akan menampilkan 'jeruk'
+echo $warna[0]; // akan menampilkan 'merah'
+print_r("<P><strong>by :jihan syafitri</strong>");
