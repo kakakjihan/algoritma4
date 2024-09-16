f98cc076d9a17af993f52ba155ad82ae498bd69c
kakakjihan <jihansahfitri6@gmail.com>
2024-09-16 13:52:05 +0700

    associative array

---
+++ 47 Melakukan Loops Pada Array Multidimensi.php
@@ -0,0 +1,7 @@
+<?php
+foreach ($films as $filmDetails) {
+    echo "Judul: " . $filmDetails[0] . "<br>";
+    echo "Tahun: " . $filmDetails[1] . "<br>";
+    echo "Sutradara: " . $filmDetails[2] . "<br><br>";
+}
+
---
+++ 48 ASSOCIATIVE ARRAY.php
@@ -0,0 +1,14 @@
+<?php
+// Menggunakan fungsi array()
+$buah = array(
+    "apel" => "<hr>hijau<hr>",
+    "strawberry" => "merah",
+    "pisang" => "kuning"
+);
+// Menetapkan langsung
+$mobil['Toyota'] = "Corolla";
+$mobil['Honda'] = "Civic<hr>";
+$mobil['Suzuki'] = "Swift";
+echo $buah["apel"]; // Output:   hijau 
+echo $mobil['Honda']; // Output: Civic
+print_r("<P><strong>by :jihan syafitri</strong>");
