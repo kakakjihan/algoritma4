c5205bb31dde03e08caa214ac26fee1ae9d6188e
kakakjihan <jihansahfitri6@gmail.com>
2024-09-17 12:24:02 +0700

    membuat array

---
+++ 60 Memperbarui Array (Update).php
@@ -0,0 +1,5 @@
+<?php
+$buah[0] = "jeruk"; // mengubah nilai elemen pertama menjadi "jeruk"
+$umur["Ali"] = 21; // mengubah umur Ali menjadi 21
+array_push($buah, "durian"); // menambah "durian" ke dalam array $buah
+$umur["Dina"] = 18; // menambah key "Dina" dengan nilai 18 ke dalam array $umur
