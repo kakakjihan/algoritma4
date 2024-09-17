2c5f2d5b32da5612ca027841a9ccd6853ebf58bd
kakakjihan <jihansahfitri6@gmail.com>
2024-09-17 12:11:39 +0700

    array merge

--- 55 array_merge().php
+++ 55 array_merge().php
@@ -1,5 +1,5 @@
 <?php
-$array1 = ["warna" => "merah", 2, 4];
+$array1 = ["warnaa" => "merah", 2, 4];
 $array2 = ["a", "b", "warna" => "hijau<br>", "bentuk" => "trapesium", 4];
 $result = array_merge($array1, $array2);
 print_r($result);
