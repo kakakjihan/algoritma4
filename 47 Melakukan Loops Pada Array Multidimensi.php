b21424bb80506bb0f807564e898bb2012ddf77ce
kakakjihan <jihansahfitri6@gmail.com>
2024-09-17 10:49:41 +0700

    array push

--- 47 Melakukan Loops Pada Array Multidimensi.php
+++ 47 Melakukan Loops Pada Array Multidimensi.php
@@ -1,7 +1,12 @@
 <?php
+$newFilm = array("Inception", 2010, "Christopher Nolan");
+$films[] = $newFilm; // Menambahkan di akhir array films
 foreach ($films as $filmDetails) {
     echo "Judul: " . $filmDetails[0] . "<br>";
     echo "Tahun: " . $filmDetails[1] . "<br>";
     echo "Sutradara: " . $filmDetails[2] . "<br><br>";
 }
+    print_r("<P><strong>by :jihan syafitri</strong>");
+
+
 
---
+++ 51 array_push.php
@@ -0,0 +1,4 @@
+<?php
+$buah = ["apel<br>", "jeruk<br>"];
+array_push($buah, "pisang<br>", "mangga<br>");
+print_r($buah);
